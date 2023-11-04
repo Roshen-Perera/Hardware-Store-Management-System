12f5e1d411a5869d0226de420b17c90de25d8aef
Roshen-Perera <roshenpilishiyanperera.com>
2023-11-04 14:10:36 +0530

    first commit

---
+++ src/main/java/lk/ijse/Jayabima/controller/LoginPageController.java
@@ -0,0 +1,73 @@
+package lk.ijse.Jayabima.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.Jayabima.db.DbConnection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginPageController {
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnForgotPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) throws IOException, SQLException {
+        String username = txtUserName.getText();
+        String password = txtPassword.getText();
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "SELECT * FROM user WHERE name = ? AND password = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/mainmenu_form.fxml"));
+                Scene scene =new Scene(rootNode);
+                Stage primaryStage = (Stage) this.rootNode.getScene().getWindow();
+                primaryStage.setScene(scene);
+                primaryStage.setTitle("Dashboard");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "oops! credentials are wrong!").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void btnSignUpFormOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/signup_form.fxml"));
+        Scene scene =new Scene(rootNode);
+        Stage primaryStage = (Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Jayabima Hardware");
+    }
+
+}
---
+++ src/main/java/lk/ijse/Jayabima/controller/SignUpPageController.java
@@ -0,0 +1,77 @@
+package lk.ijse.Jayabima.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.Jayabima.dto.SignUpDto;
+import lk.ijse.Jayabima.model.SignUpModel;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class SignUpPageController {
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtMobile;
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private PasswordField txtRepeatPassword;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    public void initialize(){
+
+    }
+    private void loadAllUser() {
+        var model = new SignUpModel();
+
+    }
+    private void clearFields() {
+        txtUserName.setText("");
+        txtMobile.setText("");
+        txtPassword.setText("");
+        txtRepeatPassword.setText("");
+    }
+
+
+
+    @FXML
+    void btnSignInFormOnAction(ActionEvent event) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+        Scene scene =new Scene(rootNode);
+        Stage primaryStage = (Stage) this.rootNode.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Jayabima Hardware");
+        primaryStage.show();
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) throws IOException {
+        String name = txtUserName.getText();
+        String mobile = txtMobile.getText();
+        String password = txtPassword.getText();
+        String repeatpassword = txtRepeatPassword.getText();
+
+        var dto = new SignUpDto(name, mobile, password, repeatpassword);
+
+        try {
+            boolean isSaved = SignUpModel.saveUser(dto);
+            if (isSaved){
+                new Alert(Alert.AlertType.CONFIRMATION,"User Saved");
+                clearFields();
+            }
+        }catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+}
